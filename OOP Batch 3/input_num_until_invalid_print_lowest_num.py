d308232df4c2b95ab4056f2e6ca664122bcb381b
Juaqine <juaqineynicoavila@gmail.com>
2025-03-15 19:51:27 +0800

    Prog04 Pseudocode

--- OOP Batch 3/input_num_until_invalid_print_lowest_num.py
+++ OOP Batch 3/input_num_until_invalid_print_lowest_num.py
@@ -1,1 +1,7 @@
 #Prog04: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the lowest number
+
+#Create an empty list that stores infinite inputs until invalid
+#Get user input as an integer
+
+#If the user input is invalid:
+#Print invalid input and the lowest number entered and stop the loop
