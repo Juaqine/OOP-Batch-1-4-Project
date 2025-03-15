8d2e9f5ff1cf48f1fc672c15177d5683fe8dff9b
Juaqine <juaqineynicoavila@gmail.com>
2025-03-15 19:41:54 +0800

    Prog10 Pseudocode

--- OOP Batch 2/input_2_num_print_num_between.py
+++ OOP Batch 2/input_2_num_print_num_between.py
@@ -1,1 +1,7 @@
 #Prog10: Create a program that ask user to input 2 numbers. Print all the numbers between the two numbers.
+
+#Ask the user to input the 1st number
+#Ask the user to input the 2nd number
+#Identify the smaller and larger number
+#Loop from the smaller number + 1 to the larger number - 1
+#Print each number in the range
