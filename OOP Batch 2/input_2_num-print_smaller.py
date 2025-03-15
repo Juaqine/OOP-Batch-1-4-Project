687ce85c2dfa2111aa8e9b979979a11305247050
Juaqine <juaqineynicoavila@gmail.com>
2025-03-15 19:24:03 +0800

    Prog01 Pseudocode

--- OOP Batch 2/input_2_num-print_smaller.py
+++ OOP Batch 2/input_2_num-print_smaller.py
@@ -1,2 +1,7 @@
 #Prog01: Create a program that ask user to input 2 numbers. Print the smaller number.
 
+#Ask the user to input the 1st number
+#Ask the user to input the 2nd number
+#Check if num1 is smaller than num2
+#Print num1 if it's smaller
+#Print num2 if not
