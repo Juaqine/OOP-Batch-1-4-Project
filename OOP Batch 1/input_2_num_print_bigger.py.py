ae529a7592189ffa1c34d0689e0b4ee194bf8e84
Juaqine <juaqineynicoavila@gmail.com>
2025-03-15 18:47:42 +0800

    Prog01 Code

--- OOP Batch 1/input_2_num_print_bigger.py.py
+++ OOP Batch 1/input_2_num_print_bigger.py.py
@@ -5,3 +5,11 @@
 #Check if num1 is greater than num2
 #Print num1 if it's greater
 #Print num2 if not
+
+num1 = float(input("Please enter the 1st number: "))
+num2 = float(input("Please enter the 2nd number: "))
+
+if num1 > num2:
+    print(f"The bigger number is: {num1}")
+else:
+    print(f"The bigger number is: {num2}")
