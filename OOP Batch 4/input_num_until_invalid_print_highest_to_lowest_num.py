44d522ab6e70b83cc51fff118c4fc89559030797
Juaqine <juaqineynicoavila@gmail.com>
2025-03-15 20:02:27 +0800

    Prog04 Code

--- OOP Batch 4/input_num_until_invalid_print_highest_to_lowest_num.py
+++ OOP Batch 4/input_num_until_invalid_print_highest_to_lowest_num.py
@@ -4,6 +4,17 @@
 #Start an infinite loop that ask for user input
 #Get user input as an integer
 
-# If the user input is invalid:
+#If the user input is invalid:
 #Goes through the list to sort the numbers entered in descending order
 #Print invalid input and the descending order of number entered and stop the loop
+
+numbers = []
+
+while True:
+    try:
+        user_input = int(input("Enter a number: "))
+        numbers.append(user_input)
+        numbers.sort(reverse=True)
+    except ValueError:
+        print("Invalid input. The descending order of what you entered is: ", numbers)
+        break
