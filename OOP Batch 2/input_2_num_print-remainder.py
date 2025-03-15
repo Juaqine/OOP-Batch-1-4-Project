7cbcf6f4ca52e4ee5574b37afe86b0e1e207c639
Juaqine <juaqineynicoavila@gmail.com>
2025-03-15 19:31:52 +0800

    Prog05 Code

--- OOP Batch 2/input_2_num_print-remainder.py
+++ OOP Batch 2/input_2_num_print-remainder.py
@@ -4,3 +4,8 @@
 #Ask the user to input the 2nd number 
 #Compute the remainder of the first number divided by the second number
 #Print the remainder
+
+num1 = float(input("Please enter the 1st number: "))
+num2 = float(input("Please enter the 2nd number: "))
+
+print(f"The remainder of the quotient is: ", num1 % num2)
