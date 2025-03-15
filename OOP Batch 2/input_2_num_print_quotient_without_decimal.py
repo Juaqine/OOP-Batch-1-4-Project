8e82d24ca8a491601f44559c40bbc444fb4de239
Juaqine <juaqineynicoavila@gmail.com>
2025-03-15 19:30:13 +0800

    Prog04 Code

--- OOP Batch 2/input_2_num_print_quotient_without_decimal.py
+++ OOP Batch 2/input_2_num_print_quotient_without_decimal.py
@@ -4,3 +4,8 @@
 #Ask the user to input the 2nd number 
 #Compute the quotient of the two inputted numbers
 #Print the quotient
+
+num1 = int(input("Please enter the 1st number: "))
+num2 = int(input("Please enter the 2nd number: "))
+
+print("The quotient of the 2 numbers is: ", num1 // num2)
