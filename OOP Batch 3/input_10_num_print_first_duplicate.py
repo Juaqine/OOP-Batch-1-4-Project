1c87b5205d1e706c907b3b0d5869b90c201f371e
Juaqine <juaqineynicoavila@gmail.com>
2025-03-15 19:47:32 +0800

    Prog02 Pseudocode

--- OOP Batch 3/input_10_num_print_first_duplicate.py
+++ OOP Batch 3/input_10_num_print_first_duplicate.py
@@ -1,1 +1,12 @@
 #Prog02: Create a program that ask user to input 10 numbers. Display all numbers. For numbers with duplicate, display only the first entry.
+
+#Create an empty list
+#Loop 10 times to get user input
+#The entered numbers are stored in the list (numbers)
+
+#Create a different list for repeating numbers
+#Check if numbers stored in both list does not repeat through looping
+#If it does repeat:
+#Add it to the 2nd list (unique_num)
+
+#Prints repeating number
