00741fed83b00d0a0a9789f00b0e7fc5e8965faf
Juaqine <juaqineynicoavila@gmail.com>
2025-03-15 19:38:29 +0800

    Prog08 Pseudocode

--- OOP Batch 2/print_all_odd_num_from_0_to_100.py
+++ OOP Batch 2/print_all_odd_num_from_0_to_100.py
@@ -1,1 +1,7 @@
 #Prog08: Create a program that print all the odd numbers starting from 0 to 100. (Use while loop)
+
+#Initialize num to 0
+#Using while loop, check if num is less than or equal to 100:
+#If num is odd:
+#Print num
+#Increase num by 1
