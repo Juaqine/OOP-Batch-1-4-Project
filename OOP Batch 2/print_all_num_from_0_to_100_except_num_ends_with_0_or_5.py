d13829268ef2db3c4dccddf72f8162cfcedb7f1e
Juaqine <juaqineynicoavila@gmail.com>
2025-03-15 19:40:38 +0800

    Prog09 Code

--- OOP Batch 2/print_all_num_from_0_to_100_except_num_ends_with_0_or_5.py
+++ OOP Batch 2/print_all_num_from_0_to_100_except_num_ends_with_0_or_5.py
@@ -5,3 +5,9 @@
 #Check if num does not end in 0 or 5
 #If true, print the number
 #Increase num by 1
+
+num = 0
+while num <= 100:
+    if num % 10 and num % 5 != 0:
+        print(num)
+    num += 1
