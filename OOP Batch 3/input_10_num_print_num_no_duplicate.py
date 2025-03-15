f0b8a5ad97051f10c60e4f56f804210b32f70c54
Juaqine <juaqineynicoavila@gmail.com>
2025-03-15 19:45:56 +0800

    Prog01 Pseudocode

--- OOP Batch 3/input_10_num_print_num_no_duplicate.py
+++ OOP Batch 3/input_10_num_print_num_no_duplicate.py
@@ -1,2 +1,12 @@
 #Prog01: Create a program that ask user to input 10 numbers. Display all numbers that don't have duplicate.
 
+#Create an empty list
+#Loop 10 times to get user input
+#The entered numbers are stored in the list (numbers)
+
+#Create a different list for not repeating numbers
+#Check if numbers stored in both list does not repeat through looping
+#If it does not repeat:
+#Add it to the 2nd list (unique_num)
+
+#Prints non repeating number
