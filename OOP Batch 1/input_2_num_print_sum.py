0273cd43f35f48d7dd01282260ba7e4ad059a6a2
Juaqine <juaqineynicoavila@gmail.com>
2025-03-15 19:03:13 +0800

    Prog03 Code

--- OOP Batch 1/input_2_num_print_sum.py
+++ OOP Batch 1/input_2_num_print_sum.py
@@ -4,3 +4,8 @@
 #Ask the user to input the 2nd number
 #Compute the sum of the two numbers
 #Print the sum
+
+num1 = float(input("Please enter the 1st number: "))
+num2 = float(input("Please enter the 2nd number: "))
+
+print("The sum of the 2 numbers is: ", num1 + num2)
