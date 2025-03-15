6c6705ca656f6d48efdece06f9daf36720b393fb
Juaqine <juaqineynicoavila@gmail.com>
2025-03-15 19:59:29 +0800

    Prog02 Code

--- OOP Batch 4/input_num_until_invalid_print_most_duplicated_num.py
+++ OOP Batch 4/input_num_until_invalid_print_most_duplicated_num.py
@@ -6,3 +6,14 @@
 #Add it to the list (numbers)
 #Create variable that checks the frequency of all the entered numbers
 #If the user input is invalid, the program will terminate and will print the most duplicated number that appeared"
+
+numbers = []
+while True:
+    try:
+        num = int(input("Enter a number (or any non-numeric character to stop): "))
+        numbers.append(num)
+        if numbers:
+            most_frequent = max(numbers, key=numbers.count)
+    except ValueError:
+        print(f"Invalid input. Most duplicated number: {most_frequent}")
+        break
