27d38b2f0c1ec7429afc81cb5682678f1ae96aa8
Juaqine <juaqineynicoavila@gmail.com>
2025-03-15 19:05:14 +0800

    Prog04 Code

--- OOP Batch 1/input_2_num_print_product.py
+++ OOP Batch 1/input_2_num_print_product.py
@@ -4,3 +4,8 @@
 #Ask the user to input the 2nd number
 #Compute the product of the two numbers
 #Print the product
+
+num1 = float(input("Please enter the 1st number: "))
+num2 = float(input("Please enter the 2nd number: "))
+
+print("The product of the 2 numbers is: ", num1 * num2)
