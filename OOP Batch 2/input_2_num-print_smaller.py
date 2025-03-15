ef4e40161499c9061a74fb52daee4e00944db66f
Juaqine <juaqineynicoavila@gmail.com>
2025-03-15 19:24:27 +0800

    Prog01 Code

--- OOP Batch 2/input_2_num-print_smaller.py
+++ OOP Batch 2/input_2_num-print_smaller.py
@@ -5,3 +5,11 @@
 #Check if num1 is smaller than num2
 #Print num1 if it's smaller
 #Print num2 if not
+
+num1 = float(input("Please enter the 1st number: "))
+num2 = float(input("Please enter the 2nd number: "))
+
+if num1 < num2:
+    print(f"The smaller number is: {num1}")
+else:
+    print(f"The smaller number is: {num2}")
