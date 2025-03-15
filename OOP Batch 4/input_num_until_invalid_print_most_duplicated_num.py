2c8f9a3716cdfae3074198a6ee6e06a92737a193
Juaqine <juaqineynicoavila@gmail.com>
2025-03-15 19:59:11 +0800

    Prog02 Pseudocode

--- OOP Batch 4/input_num_until_invalid_print_most_duplicated_num.py
+++ OOP Batch 4/input_num_until_invalid_print_most_duplicated_num.py
@@ -1,2 +1,8 @@
 #Prog02: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number with the most number of duplicate.
 
+#Create an empty list to store infinite numbers
+#Start an infinite loop
+#Get user input in integer
+#Add it to the list (numbers)
+#Create variable that checks the frequency of all the entered numbers
+#If the user input is invalid, the program will terminate and will print the most duplicated number that appeared"
