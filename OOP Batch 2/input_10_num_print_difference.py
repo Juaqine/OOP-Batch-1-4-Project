6fc8b4846bb4b3f751ff00007e98cd662c41dcaa
Juaqine <juaqineynicoavila@gmail.com>
2025-03-15 19:34:30 +0800

    Prog06 Pseudocode

--- OOP Batch 2/input_10_num_print_difference.py
+++ OOP Batch 2/input_10_num_print_difference.py
@@ -1,1 +1,9 @@
 #Prog06: Create a program that ask user to input 10 numbers. Print the result of the first number minus all of the remaining numbers.
+
+#Initialize diff to 0
+#Make the first number a variable, ask the user to enter a number
+#Loop 9 numbers to enter remaining 9 numbers:
+#Ask user to enter a number
+#Add number to diff variable
+#Subtract diff from num1
+#Print result
