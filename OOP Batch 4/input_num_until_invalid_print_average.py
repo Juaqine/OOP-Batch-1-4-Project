a780bfcc6dd44ce254714f9ee37852f32736fd78
Juaqine <juaqineynicoavila@gmail.com>
2025-03-15 20:03:39 +0800

    Prog05 Pseudocode

--- OOP Batch 4/input_num_until_invalid_print_average.py
+++ OOP Batch 4/input_num_until_invalid_print_average.py
@@ -1,2 +1,11 @@
 #Prog05: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the average.
 
+#Create an empty list that stores infinite numbers entered
+#Start an infinite loop that ask for user input
+#Get user input as an integer
+#Stores input sa list
+#Calculates the total by getting the sum of all numbers
+#Calculates the average by dividing total and the amount of entered numbers
+
+#If the user input is invalid:
+#Print invalid input and the average of numbers (w/ 2 decimals only) and stops the loop.
