9a1045a50235eec75b1ad61afc364cc27f9bd9dd
Juaqine <juaqineynicoavila@gmail.com>
2025-03-15 19:40:20 +0800

    Prog09 Pseudocode

--- OOP Batch 2/print_all_num_from_0_to_100_except_num_ends_with_0_or_5.py
+++ OOP Batch 2/print_all_num_from_0_to_100_except_num_ends_with_0_or_5.py
@@ -1,2 +1,7 @@
 #Prog09: Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero or ending five.
 
+#Initialize num to 0
+#Loop while num less than or equal to 100
+#Check if num does not end in 0 or 5
+#If true, print the number
+#Increase num by 1
