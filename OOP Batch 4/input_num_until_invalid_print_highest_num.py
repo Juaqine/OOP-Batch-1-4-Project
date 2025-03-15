a0c8bf199a5cee177c47b34a079b7f4adb9e000f
Juaqine <juaqineynicoavila@gmail.com>
2025-03-15 20:00:31 +0800

    Prog03 Pseudocode

--- OOP Batch 4/input_num_until_invalid_print_highest_num.py
+++ OOP Batch 4/input_num_until_invalid_print_highest_num.py
@@ -1,1 +1,9 @@
 #Prog03: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the highest number
+
+#Create an empty list that stores infinite inputs until invalid
+#Get user input as an integer
+#Stores the number entered on the list
+
+#If the user input is invalid:
+#The program goes through the list and check the highest number
+#Print invalid input and the highest number entered and stop the loop
