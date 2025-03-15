68fcd1c211a5bfe24c574e7b557c895bd6077a01
Juaqine <juaqineynicoavila@gmail.com>
2025-03-15 19:06:44 +0800

    Prog05 Pseudocode

--- OOP Batch 1/input_2_num_print_quotient_with_decimal.py
+++ OOP Batch 1/input_2_num_print_quotient_with_decimal.py
@@ -1,3 +1,6 @@
 #Prog05: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers with the decimal point
 
-
+#Ask the user to input the 1st number
+#Ask the user to input the 2nd number 
+#Compute the quotient of the two inputted numbers
+#Print the quotient
