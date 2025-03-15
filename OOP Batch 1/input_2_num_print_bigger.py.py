48bfa7d4fce136a60ae453ee212572da629de427
Juaqine <juaqineynicoavila@gmail.com>
2025-03-15 18:47:12 +0800

    Prog01 Pseudocode

--- OOP Batch 1/input_2_num_print_bigger.py.py
+++ OOP Batch 1/input_2_num_print_bigger.py.py
@@ -1,1 +1,7 @@
 #Prog01: Create a program that ask the user to input 2 numbers. Print the bigger number.
+
+#Ask the user to input the 1st number
+#Ask the user to input the 2nd number
+#Check if num1 is greater than num2
+#Print num1 if it's greater
+#Print num2 if not
