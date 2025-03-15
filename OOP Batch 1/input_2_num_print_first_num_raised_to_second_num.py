c576e50868496bc0cde08a015cb704358ee36b86
Juaqine <juaqineynicoavila@gmail.com>
2025-03-15 19:08:37 +0800

    Prog06 Pseudocode

--- OOP Batch 1/input_2_num_print_first_num_raised_to_second_num.py
+++ OOP Batch 1/input_2_num_print_first_num_raised_to_second_num.py
@@ -1,3 +1,6 @@
 #Prog06: Create a program that ask user to input 2 numbers. Print the result when the first number is raised to the second number.
 
-
+#Ask the user to input the 1st number
+#Ask the user to input the 2nd number
+#Computes the first inputted number raised to the exponent of the second inputted number.
+#Prints the result
