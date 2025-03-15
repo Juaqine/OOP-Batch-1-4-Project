4bcc0a62ca72bef36a2ba7b02dd7d330e3ecb4a0
Juaqine <juaqineynicoavila@gmail.com>
2025-03-15 20:02:02 +0800

    Prog04 Pseudocode

--- OOP Batch 4/input_num_until_invalid_print_highest_to_lowest_num.py
+++ OOP Batch 4/input_num_until_invalid_print_highest_to_lowest_num.py
@@ -1,1 +1,9 @@
 #Prog04: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number from highest to lowest. Clue: sort() function
+
+#Create an empty list that stores infinite numbers entered
+#Start an infinite loop that ask for user input
+#Get user input as an integer
+
+# If the user input is invalid:
+#Goes through the list to sort the numbers entered in descending order
+#Print invalid input and the descending order of number entered and stop the loop
