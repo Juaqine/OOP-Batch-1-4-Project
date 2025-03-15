a708a3efa56371d0dd42be09f1568b4758018555
Juaqine <juaqineynicoavila@gmail.com>
2025-03-15 19:26:36 +0800

    Prog02 Pseudocode

--- OOP Batch 2/input_2_num_print_not_equal.py
+++ OOP Batch 2/input_2_num_print_not_equal.py
@@ -1,1 +1,7 @@
 #Prog02: Create a program that ask user to input 2 numbers. Print "Not Equal" when the numbers are not the same.
+
+#Ask the user to input the 1st number
+#Ask the user to input the 2nd number
+#Check if both numbers are not equal
+#Print "Not Equal" if true
+#Print "Invalid" if false
