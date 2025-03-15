9d7c8b268c4ff78f5700f3e3ce72e32b93a88540
Juaqine <juaqineynicoavila@gmail.com>
2025-03-15 19:02:49 +0800

    Prog03 Pseudocode

--- OOP Batch 1/input_2_num_print_sum.py
+++ OOP Batch 1/input_2_num_print_sum.py
@@ -1,2 +1,6 @@
 #Prog03: Create a program that ask user to input 2 numbers. Print the sum of the two numbers.
 
+#Ask the user to input the 1st number
+#Ask the user to input the 2nd number
+#Compute the sum of the two numbers
+#Print the sum
